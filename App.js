b2b0d01f2e5fc6c15fbec19c4804ea13e8f0cde3
aziz_78 <aziz.fre9@gmail.com>
2023-03-06 19:02:21 +0500

    added a Title component and started work on game screen

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, ImageBackground } from "react-native";
+import { StyleSheet, ImageBackground ,SafeAreaView} from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
 import GameScreen from "./screens/GameScreen";
 export default function App() {
   const [userNumber,setUserNumber] = useState("");
-  const pickedNumberHandler= (pickedNumber) =>{
+  const pickedNumberHandler = (pickedNumber) =>{
      setUserNumber(pickedNumber);
   }
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>
@@ -17,7 +17,7 @@ export default function App() {
     
     <LinearGradient style={styles.rootScreen} colors={["#4e0329", "#ddb52f"]}>
       <ImageBackground source={require("./assets/images/background.png")} resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backgroundImage}>
-        <StartGameScreen />
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>  
       </ImageBackground>
     </LinearGradient>
   );
