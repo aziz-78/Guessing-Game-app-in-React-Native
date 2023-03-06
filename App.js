20342b5945979611392d589e136dae422d3ba8ee
aziz_78 <aziz.fre9@gmail.com>
2023-03-06 17:15:14 +0500

    added initial StartGameScreen

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, ImageBackground } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import GameScreen from "./screens/GameScreen";
+export default function App() {
+  const [userNumber,setUserNumber] = useState("");
+  const pickedNumberHandler= (pickedNumber) =>{
+     setUserNumber(pickedNumber);
+  }
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>
+  if (userNumber){
+    screen = <GameScreen/>
+  }
+  return (
+    
+    <LinearGradient style={styles.rootScreen} colors={["#4e0329", "#ddb52f"]}>
+      <ImageBackground source={require("./assets/images/background.png")} resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage:{
+    opacity:0.2,
+  }
+});
